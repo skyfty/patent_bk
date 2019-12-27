ce4d3ff2d85174cda9b9f129d577bc9e0601a318
skyfty <373388688@qq.com>
2019-12-27 11:41:29 +0800

    d

--- application/common/model/Customer.php
+++ application/common/model/Customer.php
@@ -137,8 +137,14 @@ class Customer extends Cosmetic
 
     }
 
+
     public function branch() {
         return $this->hasOne('branch','id','branch_model_id')->joinType("LEFT")->setEagerlyType(0);
+    }
+
+
+    public function adviser() {
+        return $this->hasOne('staff','id','adviser_model_id')->joinType("LEFT")->setEagerlyType(0);
     }
 
     public function getSexTextAttr($value, $data) {
