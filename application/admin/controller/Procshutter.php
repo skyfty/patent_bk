019923323f0cccdb9c88f3e3f7fef637b427bd8a
skyfty <38198234+skyfty@users.noreply.github.com>
2020-04-26 13:06:09 +0800

    Default Changelist

--- application/admin/controller/Procshutter.php
+++ application/admin/controller/Procshutter.php
@@ -19,24 +19,21 @@ class Procshutter extends Cosmetic
         $this->model = new \app\admin\model\Procshutter;
     }
 
-
     public function topdf() {
         $row = $this->model->where("id", $this->request->param("id"))->find();
         if (!$row)
             $this->error(__('No Results were found'));
 
-        \PhpOffice\PhpWord\Settings::setDefaultFontName('simsun');
-        \PhpOffice\PhpWord\Settings::setPdfRendererPath(ROOT_PATH . '/vendor/dompdf/dompdf/src');
-        \PhpOffice\PhpWord\Settings::setPdfRendererName('DomPDF');
-        $phpWord = \PhpOffice\PhpWord\IOFactory::load(ROOT_PATH . '/public' . $row['file']);
-        $xmlWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord , 'PDF');
-
         $uploadDir = "/pdfs/";
         $destFileDir =ROOT_PATH . '/public' . $uploadDir;
         if (!file_exists($destFileDir))
             mkdir($destFileDir);
-        $filename = \fast\Random::build("unique").".pdf";
-        $xmlWriter->save($destFileDir.$filename);
+        $inputfile = ROOT_PATH . '/public' . $row['file'];
+        $pdfcmd = "libreoffice --headless --convert-to pdf:writer_pdf_Export ".$inputfile." --outdir ".$destFileDir;
+        system($pdfcmd);
+        $filename = substr($row['file'], strrpos($row['file'], "/") + 1);
+        $filename = substr($filename, 0, strpos($filename, ".")).".pdf";
+        echo($uploadDir.$filename);
         $this->redirect($uploadDir.$filename);
     }
 }
