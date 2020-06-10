172e905bad5e711d2c48652610df1f294893ce42
skyfty <38198234+skyfty@users.noreply.github.com>
2020-06-10 17:16:50 +0800

    Default Changelist

--- public/assets/js/customer/invent.js
+++ public/assets/js/customer/invent.js
@@ -18,9 +18,39 @@ define(['jquery', 'bootstrap', 'fast', 'customer', 'form','flexible'], function 
 
         api: {
             bindevent: function () {
-                Form.api.bindevent($("#form"), function(data, ret){
+
+                Form.api.bindevent($("#form"),  function(data, ret){
                     setTimeout(function(){window.location.replace(ret.url);}, 500);
+                }, null, function () {
+                    if ($("#treeview").size() > 0) {
+                        var r = $("#treeview").jstree("get_all_checked");
+                        $("input[name='row[rules]']").val(r.join(','));
+                    }
+                    return true;
                 });
+
+
+                $('#row_type').select({
+                    title: "申报类型",
+                    multi: true,
+                    items: [
+                        {
+                            title: "发明",
+                            value: "faming"
+                        },
+                        {
+                            title: "实用新型",
+                            value: "xinxing"
+                        },
+                        {
+                            title: "专利合作条约（PCT）",
+                            value: "pct"
+                        }
+                    ]
+                }).on("change", function(){
+                    var values = $(this).data("values");
+                    $('[name="row[type]"]').val(values);
+                })
             }
         }
     };
