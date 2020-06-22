d1453e69faaf828deeed2e28d1f5b3c7ba07ea27
skyfty <38198234+skyfty@users.noreply.github.com>
2020-06-22 19:56:33 +0800

    Default Changelist

---
+++ application/wxapp/controller/Customer.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\wxapp\controller;
+
+use app\common\model\Fields;
+
+use think\Db;
+use app\common\controller\Wechat;
+use app\common\library\Sms as Smslib;
+
+/**
+ * CMS首页控制器
+ * Class Index
+ * @package app\wechat\controller
+ */
+class Customer extends Wxapp
+{
+
+}
